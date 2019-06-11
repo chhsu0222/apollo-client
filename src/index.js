1ec2e985fd118ff4e2372916abd29b5e54ffd41c
ch <ch@mail.startek-eng.com>
2019-06-11 10:07:29 +0800

    Apollo client in the browser: part II
    sec. 9, video 99
    
    Install Apollo boost to allow us to fire off queries and mutations.
    (https://www.apollographql.com/docs/react/essentials/get-started/)
    Install graphql to make it possible for us to define Graphql operations in Javascript.
    
    $ npm i apollo-boost@0.1.14
    $ npm i graphql@14.0.2
    
    We create a client by providing the path to our Graphql back end.
    Then we send a query with plain Javascript.
    The catch is that the query property doesn't accept a string value.
    It actually expects what's known as an abstract syntax tree,
    which represents a given Graphql operation.
    We get them generated by using gql.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-console.log('this should show up')
+import ApolloBoost, { gql } from 'apollo-boost'
+
+// Create a client
+const client = new ApolloBoost({
+    uri: 'http://localhost:4000'
+})
+
+// Send a query with plain Javascript. Remember to first import the 
+// gql function for parsing your query string into a query document.
+const getUsers = gql`
+    query {
+        users {
+            id
+            name
+        }
+    }
+`
+
+client.query({
+    query: getUsers
+}).then((response) => {
+    let html = ''
+
+    response.data.users.forEach((user) => {
+        html += `
+            <div>
+                <h3>${user.name}</h3>
+            </div>
+        `
+    })
+
+    document.getElementById('users').innerHTML = html
+})
+
+const getPosts = gql`
+    query {
+        posts {
+            title
+            author {
+                name
+            }
+        }
+    }
+`
+
+client.query({
+    query: getPosts
+}).then((response) => {
+    let html = ''
+
+    response.data.posts.forEach((post) => {
+        html += `
+            <div>
+                <h3>${post.author.name} - ${post.title}</h3>
+            </div>
+        `
+    })
+
+    document.getElementById('posts').innerHTML = html
+})
+
